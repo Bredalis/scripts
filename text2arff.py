4468ea8253871dfb84425687bb86a7dc4d19828b
PRO X <bredalisgautreaux@gmail.com>
2023-12-11 21:18:46 -0400

    text2arff.py
    Some comments.

--- text2arff.py
+++ text2arff.py
@@ -5,49 +5,51 @@ Created on Thu Oct 12 12:50:20 2017
 @author: Durgesh Samariya
 """
 
+# Import necessary libraries
 import pandas as pd
 import numpy as np
-import ntpath
+import ntpath  # Module for handling file paths
 
-#path to the dataset
+# Path to the dataset
 test = 'C:\\Users\\Dsamariya\\Desktop\\Data\\r8.txt'
 
-# define column names
+# Define column names for the DataFrame
 names = ['classLabel', 'text']
 
-# loading training data
-df = pd.read_csv( test, header=None, names=names, delimiter = "\t")
+# Load the dataset into a Pandas DataFrame
+df = pd.read_csv(test, header=None, names=names, delimiter="\t")
 
-sentences = np.array(df.text)
-classLabels = np.array(df.classLabel)
+# Extract text and class labels from the DataFrame
+sentences = np.array(df.text)  # Extract text data
+classLabels = np.array(df.classLabel)  # Extract class labels
+
+# Get directory path and file name from the provided path
 dirPath, fileName = ntpath.split(test)
 
-#myset = set(classLabels)
-
-# print(sentences[0])
-
-if fileName.endswith('.txt')== True:
+# Check if the file name ends with '.txt' extension and replace it with an empty string
+if fileName.endswith('.txt') == True:
     name = fileName.replace('.txt', '')
-    
+
+# Create the output ARFF file path and name
 outputFile = dirPath + "/" + name + ".arff"
 
+# Open the output ARFF file in write mode
 output = open(outputFile, "w")
 
-last = len(sentences)
-
-print ("Converting to ARFF file.\n")
-
+# Determine the title for the ARFF file (using the name extracted from the file path)
 title = str(name)
 
+# Write ARFF file headers and attributes
 output.write("@relation " + str(title) + "\n\n")
-
-output.write( "@attribute text string " + "\n")
-output.write( "@attribute @@class@@ " + "{" + ",".join(set(classLabels)) + "}" + "\n\n")
-
+output.write("@attribute text string " + "\n")
+output.write("@attribute @@class@@ " + "{" + ",".join(set(classLabels)) + "}" + "\n\n")
 output.write("@data" + "\n")
 
-for i in range (0, last):
-    output.write( "\'" + str(sentences[i]) + "\'" + "," + str(classLabels[i]) +  "\n" )
+# Write data instances (text and corresponding class labels) to the ARFF file
+last = len(sentences)
+for i in range(0, last):
+    output.write("\'" + str(sentences[i]) + "\'" + "," + str(classLabels[i]) + "\n")
 
+# Flush and close the output file
 output.flush()
 output.close()
